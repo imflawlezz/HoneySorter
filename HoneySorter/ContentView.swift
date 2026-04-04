01c29f12a2fd61bf23cd9f9ed0065f771b4c06af
imflawlezz <egorart6@gmail.com>
2026-04-05 01:36:34 +0200

    Initial Commit

---
+++ HoneySorter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HoneySorter
+//
+//  Created by Yahor Artsiomchyk on 05/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HoneySorter/HoneySorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneySorterApp.swift
+//  HoneySorter
+//
+//  Created by Yahor Artsiomchyk on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HoneySorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
